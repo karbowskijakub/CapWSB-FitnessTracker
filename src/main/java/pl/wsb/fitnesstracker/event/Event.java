233cd7174ef8a09fb68223c9bae91ba4386311f5
karbo <karbo360@gmail.com>
2025-11-23 15:32:42 +0100

    Lab03: Added new entities, updated DatabaseSchemaTest

--- src/main/java/pl/wsb/fitnesstracker/event/Event.java
+++ src/main/java/pl/wsb/fitnesstracker/event/Event.java
@@ -1,5 +1,54 @@
 package pl.wsb.fitnesstracker.event;
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import java.util.Date;
 
-// TODO: Define the Event entity with appropriate fields and annotations
+
+@Entity
+@Table(name = "event")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+
 public class Event {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Nullable
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @Column(name = "start_time", nullable = false)
+    private Date startTime;
+
+    @Column(name = "end_time", nullable = false)
+    private Date endTime;
+
+    @Column(nullable = false)
+    private String country;
+
+    @Column(nullable = false)
+    private String city;
+
+    public Event(
+            final String name,
+            final String description,
+            final Date startTime,
+            final Date endTime,
+            final String country,
+            final String city) {
+        this.name = name;
+        this.description = description;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.country = country;
+        this.city = city;
+    }
 }
--- src/main/java/pl/wsb/fitnesstracker/workoutsession/WorkoutSession.java
+++ src/main/java/pl/wsb/fitnesstracker/workoutsession/WorkoutSession.java
@@ -1,17 +1,60 @@
 package pl.wsb.fitnesstracker.workoutsession;
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import pl.wsb.fitnesstracker.training.api.Training;
 
-import jakarta.persistence.Id;
-
-// TODO: Define the Event entity with appropriate fields and annotations
+@Entity
+@Table(name = "workout_session")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
 public class WorkoutSession {
 
     @Id
-    private int id;
-    private int trainingId;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Nullable
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "training_id", nullable = false, unique = true)
+    private Training training;
+
+    @Column(nullable = false)
     private String timestamp;
-    private double startLatitude;
-    private double startLongitude;
-    private double endLatitude;
-    private double endLongitude;
-    private double altitude;
+
+    @Column(name = "start_latitude", nullable = false)
+    private Double startLatitude;
+
+    @Column(name = "start_longitude", nullable = false)
+    private Double startLongitude;
+
+    @Column(name = "end_latitude", nullable = false)
+    private Double endLatitude;
+
+    @Column(name = "end_longitude", nullable = false)
+    private Double endLongitude;
+
+    @Column(name = "altitude", nullable = false)
+    private Double altitude;
+
+    public WorkoutSession(
+            final Training training,
+            final String timestamp,
+            final Double startLatitude,
+            final Double startLongitude,
+            final Double endLatitude,
+            final Double endLongitude,
+            final Double altitude) {
+        this.training = training;
+        this.timestamp = timestamp;
+        this.startLatitude = startLatitude;
+        this.startLongitude = startLongitude;
+        this.endLatitude = endLatitude;
+        this.endLongitude = endLongitude;
+        this.altitude = altitude;
+    }
 }
